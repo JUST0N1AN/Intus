a24b2fe52dd365fefb528c0760e420ed19f1bb8a
shane12345601 <shane12345601@gmail.com>
2022-04-19 03:51:05 -0400

    updated about us page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="flex flex-center about">
-    <h2>About Fresher's Tech</h2>
+  <div class="row">
+    <div class="col-6 offset-3 q-pt-md">
+      <q-card dark bordered class="bg-grey-9 my-card">
+        <q-card-section>
+          <div class="text-h3" align="center">Intus</div>
+          <div class="text-h6">INFO3604: Project</div>
+          <div class="text-subtitle2">
+            by
+            <ul>
+              <li>Shane Beharry</li>
+              <li>Justin Mahabir</li>
+              <li>Carson White</li>
+              <li>Kc-Mentor Alexis</li>
+            </ul>
+          </div>
+        </q-card-section>
+
+        <q-separator dark inset />
+
+        <q-card-section align="" class="">
+          Intus is an application being designed and developed to ease the
+          process of validating persons who wishes to enter a venue without
+          physically carrying their documents required to enter that venue. In
+          recent times demand for such an application has risen given the
+          circumstances of the Covid-19 pandemic and the introduction of
+          "safe-zones"
+        </q-card-section>
+      </q-card>
+    </div>
   </div>
 </template>
+
+<style></style>
